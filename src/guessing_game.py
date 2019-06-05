acabab33d4ff6ca6296e5619ff75415649bf7695
Henrik Christensen <sensen1695@hotmail.com>
2019-06-05 22:16:52 +0200

    added mvp

--- src/guessing_game.py
+++ src/guessing_game.py
@@ -12,25 +12,50 @@ NOTE: If you strongly prefer to work locally on your own computer, you can total
 import random
 
 
-def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+MIN_GUESS = 1
+MAX_GUESS = 10
+CORRECT_NUM = random.randint(MIN_GUESS, MAX_GUESS)
 
+
+def welcome():
+    print('Welcome to the Number Guessing Game!')
+    print('------------------------------------')
+    print(f'Guess a number between {MIN_GUESS} and {MAX_GUESS}\n')
+
+
+def prompt_for_guess():
+    while True:
+        guess = input('Enter a number: ')
+        try:
+            guess = int(guess)
+
+            if guess < MIN_GUESS or guess > MAX_GUESS:
+                raise ValueError(f'Please only enter a number between {MIN_GUESS} and {MAX_GUESS}')
+        except ValueError as err:
+            print(f'Invalid input: {err}')
+        else:
+            return guess
+
+
+def start_game():
+    num_guesses = 0
+    still_guessing = True
+
+    welcome()
+
+    while still_guessing:
+        num_guesses += 1
+        guess = prompt_for_guess()
+
+        if guess == CORRECT_NUM:
+            print(f'\nCongratulations! You guesses the number in {num_guesses} tries!')
+            still_guessing = False
+        elif guess < CORRECT_NUM:
+            print('The number is higher')
+        else:
+            print('The number is lower')
+    print('Goodbye\n')
+    
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
